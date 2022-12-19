007eeb574b0b412a99c0f6b5ddebdfe25ef57314
Anibal <joan.maldonadoa@gmail.com>
2022-12-18 22:27:06 -0300

    Modificación del estilado del footer

--- src/ui/Footer/Footer.jsx
+++ src/ui/Footer/Footer.jsx
@@ -1,5 +1,24 @@
 import "./Footer.css";
 
 export const Footer = () => {
-  return <div className="footer--css">FOOTER</div>;
+  return (
+    <div className="footer--css">
+      <div className="titulo--tienda container">
+        <img src="static/miniatura.png" className="img--footer"></img>
+        <h1>MiniShop</h1>
+        <h5>2022</h5>
+        <div className="iconos--redes">
+          <img src="static/img/facebook.png"></img>
+          <img src="static/img/instagram.png"></img>
+          <img src="static/img/twitter.png"></img>
+        </div>
+
+        <div>
+          <p>HOLA</p>
+          <p>CHAO</p>
+          <p>BUENAS TARDES</p>
+        </div>
+      </div>
+    </div>
+  );
 };
