0b3b5524ef17f02211d28ae0fa418a4e7d021baf
Anibal <joan.maldonadoa@gmail.com>
2022-12-18 22:26:23 -0300

    Eliminación de archivos por refactorización

--- src/services/getModeloByLinea.js
+++
@@ -1,5 +0,0 @@
-import { inventario } from "../data/inventario";
-
-export const getModeloByLinea = (linea) => {
-  return inventario.filter((modelo) => modelo.linea === linea);
-};
--- src/services/getModeloBySku.js
+++
@@ -1,5 +0,0 @@
-import { inventario } from "../data/inventario";
-
-export const getModeloBySku = (sku) => {
-  return inventario.find((modelo) => modelo.sku === sku);
-};
